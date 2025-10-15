6f85b2292a87e479dc08a07b5f6dd3f22cfcc4a7
Ishan <211b145@juetguna.in>
2025-10-15 22:21:25 +0530

    update api.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,14 @@ import axios from "axios";
 const API_BASE = "http://127.0.0.1:8000/api";
 
 export const getCandidates = async (filters = {}) => {
-  const params = new URLSearchParams(filters).toString();
+  // ✅ Set default year to 2019 if not provided
+  const queryFilters = {
+    year: filters.year || "2019",
+    ...filters,
+  };
+
+  const params = new URLSearchParams(queryFilters).toString();
+
   const res = await axios.get(`${API_BASE}/candidates/?${params}`);
   return res.data;
 };
